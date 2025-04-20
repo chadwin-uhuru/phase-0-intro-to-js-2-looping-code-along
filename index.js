56828bb4cac44b8bc9444b762fef53b02021164c
chadwin-uhuru <uhuruchadwin166@gmail.com>
2025-04-20 09:14:30 +0300

    Complete Looping Lab with writeCards and countDown functions

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+// WriteCards function
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+  }
+  
+  // CountDown function
+  function countDown(start) {
+    while (start >= 0) {
+      console.log(start);
+      start--;
+    }
+  }
+  
